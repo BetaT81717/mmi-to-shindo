63c8618f812a41d5d5ac3f4e8347d5ba7c2cdafb
Alexander Khmelnov <160534162+BetaT81717@users.noreply.github.com>
2026-09-06 00:33:28 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,40 +7,40 @@ document.getElementById('msub').onclick = function(){
 		if (mmi.includes(mmival)){
 			if (mmival === "I"){
 				console.log(mmival)
-				document.getElementById('shindoin').value = "0"
+				document.getElementById('shindo').value = "0"
 			}else if (mmival === "II"){
 				console.log(mmival)
-				document.getElementById('shindoin').value = "1"
+				document.getElementById('shindo').value = "1"
 			}else if (mmival === "III"){
 				console.log(mmival)
-				document.getElementById('shindoin').value = "2"
+				document.getElementById('shindo').value = "2"
 			}else if (mmival === "IV"){
 				console.log(mmival)
-				document.getElementById('shindoin').value = "3"
+				document.getElementById('shindo').value = "3"
 			}else if (mmival === "V"){
 				console.log(mmival)
-				document.getElementById('shindoin').value = "4"
+				document.getElementById('shindo').value = "4"
 			}else if (mmival === "VI"){
 				console.log(mmival)
-				document.getElementById('shindoin').value = "5-"
+				document.getElementById('shindo').value = "5-"
 			}else if (mmival === "VII"){
 				console.log(mmival)
-				document.getElementById('shindoin').value = "5+"
+				document.getElementById('shindo').value = "5+"
 			}else if (mmival === "VIII"){
 				console.log(mmival)
-				document.getElementById('shindoin').value = "6-"
+				document.getElementById('shindo').value = "6-"
 			}else if (mmival === "IX"){				
 				console.log(mmival)
-				document.getElementById('shindoin').value = "6+"
+				document.getElementById('shindo').value = "6+"
 			}else if (mmival === "X"){				
 				console.log(mmival)
-				document.getElementById('shindoin').value = "6+"
+				document.getElementById('shindo').value = "6+"
 			}else if (mmival === "XI"){				
 				console.log(mmival)
-				document.getElementById('shindoin').value = "7"
+				document.getElementById('shindo').value = "7"
 			}else if (mmival === "XII"){				
 				console.log(mmival)
-				document.getElementById('shindoin').value = "7"	
+				document.getElementById('shindo').value = "7"	
 			}
 			else {
 				console.log(`Error: Type or value error`)
@@ -52,7 +52,7 @@ document.getElementById('msub').onclick = function(){
 }
 document.getElementById('ssub').onclick = function(){
 	document.getElementById('errordisp').textContent = ""
-	let shindoval = document.getElementById('shindoin').value
+	let shindoval = document.getElementById('shindo').value
 		if (shindo.includes(shindoval)){
 			if (shindoval === "0"){
 				console.log(shindoval)
